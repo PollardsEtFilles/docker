5cf55efa2c6d9504ff78fd9ccd00278cf33124e5
Peter Lappo <peter.lappo@smr.co.uk>
2018-05-03 23:56:43 +0100

    Use version

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
+#! /bin/bash
 
 version=2.119
 sha=$(curl -q -fsSL "https://repo.jenkins-ci.org/releases/org/jenkins-ci/main/jenkins-war/${version}/jenkins-war-${version}.war.sha256" )
-echo $sha
 
 docker build --file "Dockerfile" \
                  --build-arg "JENKINS_VERSION=$version" \
                  --build-arg "JENKINS_SHA=$sha" \
-                 --tag "pef/jenkins:1.0.0" .
+                 --tag "pef/jenkinsbase:latest" \
+                 --tag "pef/jenkinsbase:$version" .
 
